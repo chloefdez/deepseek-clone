db06d435e0532052e7172fca336554b77288543f
Chloe <chloefdez@gmail.com>
2025-07-31 19:58:24 -0700

    fixed db.js to properly connect to mongodb

--- config/db.js
+++ config/db.js
@@ -1,16 +1,29 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose || {conn: null, promise: null}
+let cached = global.mongoose || { conn: null, promise: null };
 
 export default async function connectDB() {
-    if(cached.conn) return cached.conn;
-    if(!cached.promise) {
-        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
-    }
-    try {
-        cached.conn = await cached.promise;
-    } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
-    }
-    return cached.conn 
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, {
+        dbName: "deepseek",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => {
+        console.log("✅ MongoDB connected");
+        return mongoose;
+      });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    console.log("❌ Error connecting to MongoDB:", error);
+  }
+
+  global.mongoose = cached;
+  return cached.conn;
 }
