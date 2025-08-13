1ea419e603444e69fd4d43a2baab4d03a1b2e398
Chloe <chloefdez@gmail.com>
2025-08-12 19:38:56 -0700

    removed unused vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 import { ClerkProvider } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
@@ -11,29 +10,31 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+export const metadata: Metadata = {
+  title: "Deepseek Clone",
+  description: "Full Stack Project",
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
-      <AppContextProvider>
-        <html lang="en">
-          <Head>
-            <title>Deepseek Clone</title>
-            <meta name="description" content="Full Stack Project" />
-          </Head>
-          <body className={`${inter.className} antialiased h-screen w-screen`}>
-            <Toaster toastOptions={
-              {
-                success: {style: { background: "black", color: "white" }},
-                error: {style: { background: "black", color: "white" }}
-              }
-            } />
-            {children}</body>
-        </html>
-      </AppContextProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased h-screen w-screen`}>
+          <AppContextProvider>
+            <Toaster
+              toastOptions={{
+                success: { style: { background: "black", color: "white" } },
+                error: { style: { background: "black", color: "white" } },
+              }}
+            />
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [expand, setExpand] = useState(false);
-  const [messages, setMessages] = useState([]);
+  const [messages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   return (
